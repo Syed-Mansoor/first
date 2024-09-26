f48a8335c001a8d05ae04d733a07b57f84b901cb
Syed-mansoor <saeedmansoor56@gmail.com>
2024-09-26 14:29:33 +0530

    updated Exception.py

--- diamond/exception/exception.py
+++ diamond/exception/exception.py
@@ -1,25 +1,24 @@
+import os
 import sys
 
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
 
-
+    return error_message
 
 class DiamondException(Exception):
+    def __init__(self, error_message, error_detail):
+        """
+        :param error_message: error message in string format
+        """
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
 
-    def __init__(self,error_message,error_details:sys):
-        self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-        self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
-
-        
     def __str__(self):
-        return 'Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(self.file_name,self.lineno,str(self.error_message))
-    
-
-
-
-if __name__ == "__main__":
-    try:
-        s = 2/0
-    except Exception as e :
-        raise DiamondException(e, sys)
+        return self.error_message
