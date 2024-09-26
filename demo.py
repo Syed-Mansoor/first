0c9b72070b58c0b02885a56c2e631d6cd90270db
Syed-mansoor <saeedmansoor56@gmail.com>
2024-09-26 15:46:01 +0530

    training pipeline added

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
 from diamond.exception.exception import DiamondException
 from diamond.logger import logging
 import sys
-try:
-    a = 1/0
-    logging.info("Error occured here")
-except Exception as e:
-    raise DiamondException(e,sys)
+logging.info("haha")
---
+++ diamond/pipeline/training_pipeline.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from diamond.logger import logging
+from diamond.exception.exception import DiamondException
+import pandas as pd
+
+from diamond.component.data_ingestion import DataIngestion
+from diamond.component.data_transformation import DataTransformation
+from diamond.component.model_trainer import ModelTrainer
+
+
+
+obj=DataIngestion()
+
+train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+data_transformation=DataTransformation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
